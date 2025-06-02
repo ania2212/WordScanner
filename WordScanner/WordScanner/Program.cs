4becfef0b5a869ea60b0248bc72be1c35c1ed5e7
Anna Tyshchenko <annaalexeyenko42@gmail.com>
2025-06-02 11:48:08 +0300

    Add basic configuration support

--- WordScanner/WordScanner/Program.cs
+++ WordScanner/WordScanner/Program.cs
@@ -1,13 +1,26 @@
-﻿using WordScanner.Factories;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+using WordScanner.Factories;
 using WordScanner.Services;
 using WordScanner.UI;
 using WordScanner.WordAnalysis;
 
-string _ignoreWordsPath = @"D:\WordScanner\WordScanner\WordScanner\Resources\ignorewords.txt";
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false)
+    .Build();
+
+var ignoreWordsPath = configuration["IgnoreWordsPath"];
+if (string.IsNullOrWhiteSpace(ignoreWordsPath))
+{
+    Console.WriteLine("IgnoreWordsPath was not set");
+    return;
+}
+
 StatisticsCollector _wordStatistics = new();
 FileProcessorFactory _fileProcessorFactory = new();
 FileProcessingService _fileProcessingService = new(_wordStatistics, _fileProcessorFactory);
 
-var ui = new UIHandler(_wordStatistics, _ignoreWordsPath, _fileProcessingService);
+var ui = new UIHandler(_wordStatistics, ignoreWordsPath, _fileProcessingService);
 
 ui.RunConsoleLoop();
